af8fe545f37f3e66f3869b3684a3d5d784ddbe76
OusmaneKana <kanaman6@gmail.com>
2023-02-23 00:20:29 -0600

    Update test cases

--- final_project/machinetranslation/tests/test.py
+++ final_project/machinetranslation/tests/test.py
@@ -6,12 +6,13 @@ agent = WatsonTranslator()
 class TestingENToFR(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(agent.englishToFrench("Hello"),"Bonjour")
-
-
-
+		self.assertNotEqual(agent.englishToFrench("Hello"),"Dormir")
+		
 class TestingFRToEN(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(agent.frenchToEnglish("Bonjour"),"Hello")
+		self.assertNotEqual(agent.frenchToEnglish("Bonjour"),"Good Bye")
+
 
 
 
