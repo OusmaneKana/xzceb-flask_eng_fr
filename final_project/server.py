b5b22dd621580aca78c47f23e54690fa64e63456
OusmaneKana <kanaman6@gmail.com>
2023-02-23 00:17:48 -0600

    Last touches

--- final_project/server.py
+++ final_project/server.py
@@ -3,24 +3,24 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask("Web Translator")
-translator = WatsonTranslator()
+agent = translator.WatsonTranslator()
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    translated_to_french = translator.englishToFrench(textToTranslate)
+    translated_to_french = agent.english_to_french(textToTranslate)
     return translated_to_french
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    translated_to_english = translator.englishToFrench(textToTranslate)
+    translated_to_english = agent.french_to_english(textToTranslate)
     return translated_to_english
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
     return render_template("index.html")
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
